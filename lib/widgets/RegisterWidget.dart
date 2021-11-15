791e2f849ccd2da1f9442ee045eb2075fb048f3c
kay1C <kay1C@users.noreply.github.com>
2021-11-16 00:27:38 +0300

    RegisterWidget.dart updated with textfields validation

--- lib/widgets/RegisterWidget.dart
+++ lib/widgets/RegisterWidget.dart
@@ -1,6 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
-class RegisterWidget extends StatelessWidget {
+class RegisterWidget extends StatefulWidget {
   final String? title;
   final VoidCallback? onPressed;
   final Widget? child;
@@ -11,16 +13,110 @@ class RegisterWidget extends StatelessWidget {
       : super(key: key);
 
   @override
+  _RegisterFormState createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterWidget> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title ?? 'Register'),
-      ),
-      body: Center(
-        child: Column(children: [
-          Text("Register page"),
-          ElevatedButton(onPressed: onPressed, child: Text('go back'))
-        ]),
+      body: SafeArea(
+        child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.always,//automatic validation
+            child: Expanded(
+              child: Column(children: <Widget>[
+                TextFormField(
+                  validator: (value) {
+                    //email regex validation
+                    //bool emailValid = RegExp( r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value ?? '');
+                    if ((value == null || value.isEmpty)) {
+                      //value can be null or empty
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Enter your email',
+                    icon: Icon(Icons.email),
+                  ),
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      //value can be null or empty
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                    icon: Icon(Icons.lock),
+                  ),
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      //value can be null or empty
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Confirm Password',
+                    hintText: 'Enter your password',
+                    icon: Icon(Icons.lock),
+                  ),
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      //value can be null or empty
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                    hintText: 'Enter your name',
+                    icon: Icon(Icons.person),
+                  ),
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      //value can be null or empty
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Phone',
+                    hintText: 'Enter your phone',
+                    icon: Icon(Icons.phone),
+                  ),
+                ),
+                //send form
+                ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false otherwise.
+                    if (_formKey.currentState!.validate()) {
+                      // If the form is valid, display a snackbar. In the real world,
+                      // you'd often call a server or save the information in a database.
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')),
+                        
+                      );
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ]),
+            )),
       ),
     );
   }
